63aa6d285d63983d1cf6b2d58c8771f987c4bd67
teleskop150750 <teleskop150750@gmail.com>
2020-10-14 16:10:48 +0300

    рефакторинг

--- script.js
+++ script.js
@@ -1,174 +1,95 @@
 // Dynamic Adapt v.1
 // HTML data-da="where(uniq class name),position(digi),when(breakpoint)"
-// e.x. data-da="item,2,992"
+// e.x. data-da="item,769,last,min"
 // Andrikanych Yevhen 2020
 // https://www.youtube.com/c/freelancerlifestyle
 
-"use strict";
+const daElements = document.querySelectorAll('[data-da]');
+const daElementsArray = [];
+let mediaArray = [];
 
-(function () {
-	let originalPositions = [];
-	let daElements = document.querySelectorAll('[data-da]');
-	let daElementsArray = [];
-	let daMatchMedia = [];
-	//Заполняем массивы
-	if (daElements.length > 0) {
-		let number = 0;
-		for (let index = 0; index < daElements.length; index++) {
-			const daElement = daElements[index];
-			const daMove = daElement.getAttribute('data-da');
-			if (daMove != '') {
-				const daArray = daMove.split(',');
-				const daPlace = daArray[1] ? daArray[1].trim() : 'last';
-				const daBreakpoint = daArray[2] ? daArray[2].trim() : '767';
-				const daType = daArray[3] === 'min' ? daArray[3].trim() : 'max';
-				const daDestination = document.querySelector('.' + daArray[0].trim())
-				if (daArray.length > 0 && daDestination) {
-					daElement.setAttribute('data-da-index', number);
-					//Заполняем массив первоначальных позиций
-					originalPositions[number] = {
-						"parent": daElement.parentNode,
-						"index": indexInParent(daElement)
-					};
-					//Заполняем массив элементов 
-					daElementsArray[number] = {
-						"element": daElement,
-						"destination": document.querySelector('.' + daArray[0].trim()),
-						"place": daPlace,
-						"breakpoint": daBreakpoint,
-						"type": daType
-					}
-					number++;
-				}
-			}
-		}
-		dynamicAdaptSort(daElementsArray);
+// Функция получения индекса внутри родителя
+const indexInParent = (parent, el) => [...parent.children].indexOf(el);
 
-		//Создаем события в точке брейкпоинта
-		for (let index = 0; index < daElementsArray.length; index++) {
-			const el = daElementsArray[index];
-			const daBreakpoint = el.breakpoint;
-			const daType = el.type;
+// Сортировка массива по возрастанию
+const dynamicAdaptSort = (arr) => {
+  arr.sort((a, b) => {
+    if (a.breakpoint === b.breakpoint) {
+      if (a.place === b.place) {
+        return 0;
+      }
+      if (a.place === 'first' || b.place === 'last') {
+        return -1;
+      }
+      if (a.place === 'last' || b.place === 'first') {
+        return 1;
+      }
+      return a.place - b.place;
+    }
+    return a.breakpoint - b.breakpoint;
+  });
+};
 
-			daMatchMedia.push(window.matchMedia("(" + daType + "-width: " + daBreakpoint + "px)"));
-			daMatchMedia[index].addListener(dynamicAdapt);
-		}
-	}
-	//Основная функция
-	function dynamicAdapt(e) {
-		for (let index = 0; index < daElementsArray.length; index++) {
-			const el = daElementsArray[index];
-			const daElement = el.element;
-			const daDestination = el.destination;
-			const daPlace = el.place;
-			const daBreakpoint = el.breakpoint;
-			const daClassname = "_dynamic_adapt_" + daBreakpoint;
+daElements.forEach((daElement, id) => {
+  const daData = daElement.dataset.da.trim();
+  if (daData !== '') {
+    const object = {};
+    const daMove = daData.split(',');
+    object.id = id;
+    object.parent = daElement.parentNode;
+    object.element = daElement;
 
-			if (daMatchMedia[index].matches) {
-				//Перебрасываем элементы
-				if (!daElement.classList.contains(daClassname)) {
-					let actualIndex = indexOfElements(daDestination)[daPlace];
-					if (daPlace === 'first') {
-						actualIndex = indexOfElements(daDestination)[0];
-					} else if (daPlace === 'last') {
-						actualIndex = indexOfElements(daDestination)[indexOfElements(daDestination).length];
-					}
-					daDestination.insertBefore(daElement, daDestination.children[actualIndex]);
-					daElement.classList.add(daClassname);
-				}
-			} else {
-				//Возвращаем на место
-				if (daElement.classList.contains(daClassname)) {
-					dynamicAdaptBack(daElement);
-					daElement.classList.remove(daClassname);
-				}
-			}
-		}
-		customAdapt();
-	}
+    object.destination = document.querySelector(`.${daMove[0].trim()}`);
+    object.breakpoint = daMove[1] ? daMove[1].trim() : '769';
+    object.place = daMove[2] ? daMove[2].trim() : 'last';
+    object.type = daMove[3] ? daMove[3].trim() : 'min';
 
-	//Вызов основной функции
-	dynamicAdapt();
-
-	//Функция возврата на место
-	function dynamicAdaptBack(el) {
-		const daIndex = el.getAttribute('data-da-index');
-		const originalPlace = originalPositions[daIndex];
-		const parentPlace = originalPlace['parent'];
-		const indexPlace = originalPlace['index'];
-		const actualIndex = indexOfElements(parentPlace, true)[indexPlace];
-		parentPlace.insertBefore(el, parentPlace.children[actualIndex]);
-	}
-	//Функция получения индекса внутри родителя
-	function indexInParent(el) {
-		var children = Array.prototype.slice.call(el.parentNode.children);
-		return children.indexOf(el);
-	}
-	//Функция получения массива индексов элементов внутри родителя 
-	function indexOfElements(parent, back) {
-		const children = parent.children;
-		const childrenArray = [];
-		for (let i = 0; i < children.length; i++) {
-			const childrenElement = children[i];
-			if (back) {
-				childrenArray.push(i);
-			} else {
-				//Исключая перенесенный элемент
-				if (childrenElement.getAttribute('data-da') == null) {
-					childrenArray.push(i);
-				}
-			}
-		}
-		return childrenArray;
-	}
-	//Сортировка объекта
-	function dynamicAdaptSort(arr) {
-		arr.sort(function (a, b) {
-			if (a.breakpoint > b.breakpoint) { return -1 } else { return 1 }
-		});
-		arr.sort(function (a, b) {
-			if (a.place > b.place) { return 1 } else { return -1 }
-		});
-	}
-	//Дополнительные сценарии адаптации
-	function customAdapt() {
-		//const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	}
-}());
-
-/*
-let block = document.querySelector('.click');
-block.addEventListener("click", function (e) {
-	alert('Все ок ;)');
+    daElementsArray.push(object);
+  }
 });
-*/
 
-/*
-//Объявляем переменные
-const parent_original = document.querySelector('.content__blocks_city');
-const parent = document.querySelector('.content__column_river');
-const item = document.querySelector('.content__block_item');
+dynamicAdaptSort(daElementsArray);
 
-//Слушаем изменение размера экрана
-window.addEventListener('resize', move);
+const dynamicAdaptTo = (place, element, destination) => {
+  if (place === 'last' || place >= destination.children.length) {
+    destination.append(element);
+    return;
+  }
+  if (place === 'first') {
+    destination.prepend(element);
+    return;
+  }
+  destination.children[place].before(element);
+};
+const dynamicAdaptBack = (parent, element, index) => {
+  if (parent.children[index] === undefined) {
+    parent.append(element);
+    return;
+  }
+  parent.children[index].before(element);
+};
 
-//Функция
-function move(){
-	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	if (viewport_width <= 992) {
-		if (!item.classList.contains('done')) {
-			parent.insertBefore(item, parent.children[2]);
-			item.classList.add('done');
-		}
-	} else {
-		if (item.classList.contains('done')) {
-			parent_original.insertBefore(item, parent_original.children[2]);
-			item.classList.remove('done');
-		}
-	}
-}
+const dynamicAdapt = (media, br) => {
+  const array = daElementsArray.filter(({ breakpoint }) => breakpoint === br);
+  if (media.matches) {
+    array.forEach((item) => {
+      item.index = indexInParent(item.parent, item.element);
+      dynamicAdaptTo(item.place, item.element, item.destination);
+    });
+  } else {
+    array.forEach((item) => {
+      dynamicAdaptBack(item.parent, item.element, item.index);
+    });
+  }
+};
 
-//Вызываем функцию
-move();
-
-*/
+mediaArray = daElementsArray
+  .map(({ type, breakpoint }) => `(${type}-width: ${breakpoint}px),${breakpoint}`)
+  .filter((item, index, self) => self.indexOf(item) === index)
+  .forEach((item) => {
+    const arr = item.split(',');
+    const media = window.matchMedia(arr[0]);
+    const breakpoint = arr[1];
+    media.addEventListener('change', dynamicAdapt.bind(null, media, breakpoint));
+    dynamicAdapt.call(null, media, breakpoint);
+  });
