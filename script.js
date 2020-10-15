9c7da9d7a48fb6373cf8ad03b170e13808b8208f
teleskop150750 <teleskop150750@gmail.com>
2020-10-15 15:53:17 +0300

    fix

--- script.js
+++ script.js
@@ -5,117 +5,136 @@
 // https://www.youtube.com/c/freelancerlifestyle
 
 class DynamicAdapt {
-	// массив объектов
-	elementsArray = [];
+  // массив объектов
+  elementsArray = [];
+  daClassname = '_dynamic_adapt_';
 
-	init() {
-		// массив DOM-элементов
-		this.elements = [...document.querySelectorAll('[data-da]')];
+  constructor(type) {
+    this.type = type;
+  }
 
-		// наполнение elementsArray объктами
-		this.elements.forEach((element) => {
-			const data = element.dataset.da.trim();
-			if (data !== '') {
-				const dataArray = data.split(',');
+  init() {
+    // массив DOM-элементов
+    this.elements = [...document.querySelectorAll('[data-da]')];
 
-				const oElement = {};
-				oElement.element = element;
-				oElement.parent = element.parentNode;
-				oElement.destination = document.querySelector(`.${dataArray[0].trim()}`);
-				oElement.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
-				oElement.place = dataArray[2] ? dataArray[2].trim() : 'last';
-				oElement.type = dataArray[3] ? dataArray[3].trim() : 'max';
+    // наполнение elementsArray объктами
+    this.elements.forEach((element) => {
+      const data = element.dataset.da.trim();
+      if (data !== '') {
+        const dataArray = data.split(',');
 
-				this.elementsArray.push(oElement);
-			}
-		});
+        const oElement = {};
+        oElement.element = element;
+        oElement.parent = element.parentNode;
+        oElement.destination = document.querySelector(`.${dataArray[0].trim()}`);
+        oElement.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
+        oElement.place = dataArray[2] ? dataArray[2].trim() : 'last';
 
-		this.arraySort(this.elementsArray);
+        oElement.index = this.indexInParent(
+          oElement.parent, oElement.element,
+        );
 
-		// массив уникальных медиа-запросов
-		this.mediaArray = this.elementsArray
-			.map(({ type, breakpoint }) => `(${type}-width: ${breakpoint}px),${breakpoint}`)
-			.filter((item, index, self) => self.indexOf(item) === index);
+        this.elementsArray.push(oElement);
+      }
+    });
 
-		// навешивание слушателя на медиа-запрос
-		// и вызов обработчика при первом запуске
-		this.mediaArray.forEach((item) => {
-			const itemSplit = item.split(',');
-			const media = window.matchMedia(itemSplit[0]);
-			const breakpoint = itemSplit[1];
-			media.addEventListener('change', this.mediaHandler.bind(this, media, breakpoint));
-			this.mediaHandler.call(this, media, breakpoint);
-		});
-	}
+    this.arraySort(this.elementsArray);
 
-	// Основная функция
-	mediaHandler(media, breakpointMeida) {
-		// массив объектов с подходящим брейкпоинтом
-		const elementsFilter = this.elementsArray.filter(
-			({ breakpoint }) => breakpoint === breakpointMeida,
-		);
+    // массив уникальных медиа-запросов
+    this.mediaArray = this.elementsArray
+      .map(({ breakpoint }) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)
+      .filter((item, index, self) => self.indexOf(item) === index);
 
-		if (media.matches) {
-			elementsFilter.forEach((oElement) => {
-				// получение индекса внутри родителя
-				oElement.index = this.indexInParent(
-					oElement.parent, oElement.element,
-				);
-				this.moveTo(oElement.place, oElement.element, oElement.destination);
-			});
-		} else {
-			elementsFilter.forEach(({ parent, element, index }) => {
-				this.moveBack(parent, element, index);
-			});
-		}
-	}
+    // навешивание слушателя на медиа-запрос
+    // и вызов обработчика при первом запуске
+    this.mediaArray.forEach((item) => {
+      const mediaSplit = item.split(',');
+      const mediaQuerie = window.matchMedia(mediaSplit[0]);
+      const mediaBreakpoint = mediaSplit[1];
 
-	// Функция перемещения
-	moveTo(place, element, destination) {
-		if (place === 'last' || place >= destination.children.length) {
-			destination.append(element);
-			return;
-		}
-		if (place === 'first') {
-			destination.prepend(element);
-			return;
-		}
-		destination.children[place].before(element);
-	}
+      // массив объектов с подходящим брейкпоинтом
+      const elementsFilter = this.elementsArray.filter(
+        ({ breakpoint }) => breakpoint === mediaBreakpoint
+      );
+      mediaQuerie.addEventListener('change', () => {
+        this.mediaHandler(mediaQuerie, elementsFilter);
+      });
+      this.mediaHandler(mediaQuerie, elementsFilter);
+    });
+  }
 
-	// Функция возврата
-	moveBack(parent, element, index) {
-		if (parent.children[index] === undefined) {
-			parent.append(element);
-			return;
-		}
-		parent.children[index].before(element);
-	}
+  // Основная функция
+  mediaHandler(mediaQuerie, elementsFilter) {
+    if (mediaQuerie.matches) {
+      elementsFilter.forEach((oElement) => {
+        // получение индекса внутри родителя
+        oElement.index = this.indexInParent(
+          oElement.parent, oElement.element,
+        );
+        this.moveTo(oElement.place, oElement.element, oElement.destination);
+      });
+    } else {
+      elementsFilter.forEach(({ parent, element, index }) => {
+        if (element.classList.contains(this.daClassname)) {
+          this.moveBack(parent, element, index);
+        }
+      });
+    }
+  }
 
-	// Функция получения индекса внутри родителя
-	indexInParent(parent, element) {
-		return [...parent.children].indexOf(element);
-	}
+  // Функция перемещения
+  moveTo(place, element, destination) {
+    element.classList.add(this.daClassname);
+    if (place === 'last' || place >= destination.children.length) {
+      destination.append(element);
+      return;
+    }
+    if (place === 'first') {
+      destination.prepend(element);
+      return;
+    }
+    destination.children[place].before(element);
+  }
 
-	// Функция сортировки массива по breakpoint и place по возрастанию
-	arraySort(arr) {
-		arr.sort((a, b) => {
-			if (a.breakpoint === b.breakpoint) {
-				if (a.place === b.place) {
-					return 0;
-				}
-				if (a.place === 'first' || b.place === 'last') {
-					return -1;
-				}
-				if (a.place === 'last' || b.place === 'first') {
-					return 1;
-				}
-				return a.place - b.place;
-			}
-			return a.breakpoint - b.breakpoint;
-		});
-	}
+  // Функция возврата
+  moveBack(parent, element, index) {
+    element.classList.remove(this.daClassname);
+    if (parent.children[index] === undefined) {
+      parent.append(element);
+      return;
+    }
+    parent.children[index].before(element);
+  }
+
+  // Функция получения индекса внутри родителя
+  indexInParent(parent, element) {
+    return [...parent.children].indexOf(element);
+  }
+
+  // Функция сортировки массива по breakpoint и place по возрастанию
+  arraySort(arr) {
+    arr.sort((a, b) => {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.place === b.place) {
+          return 0;
+        }
+        if (a.place === 'first' || b.place === 'last') {
+          return -1;
+        }
+        if (a.place === 'last' || b.place === 'first') {
+          return 1;
+        }
+        return a.place - b.place;
+      }
+      if (this.type === 'max') {
+        return b.breakpoint - a.breakpoint;
+      }
+      if (this.type === 'min') {
+        return a.breakpoint - b.breakpoint;
+      }
+    });
+  }
 }
 
-const da = new DynamicAdapt();
+const da = new DynamicAdapt('max');
 da.init();
