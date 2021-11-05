678d01b2d91d998776c45e221d44df336553a959
Andrey Yudin <info@ivc-inform.ru>
2021-11-05 17:23:13 +0300

    .

---
+++ gulpfile.ts
@@ -0,0 +1,200 @@
+import { fontsStl } from "./src/ts/functions";
+
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const cleanCss = require("gulp-clean-css");
+const del = require("del");
+const fileInclude = require("gulp-file-include");
+const fonter = require("gulp-fonter-unx");
+const fs = require("fs");
+const groupMedia = require("gulp-group-css-media-queries");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const scss = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const buffer = require("vinyl-buffer");
+const svgSprite = require("gulp-svg-sprite");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const ttf2woff = require("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const uglify = require("gulp-uglify-es").default;
+const webp = require("gulp-webp");
+const webpHtml = require("gulp-webp-html");
+const webpcss = require("gulp-webpcss");
+const browserify = require("browserify");
+const sourceify = require("sourceify");
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
+const {dest, src, series, parallel, watch, task} = require("gulp");
+
+const projectFolder = require("path").basename(__dirname);
+const srcFolder = "src";
+
+const path = {
+    build: {
+        html: `${projectFolder}/`,
+        css: `${projectFolder}/css/`,
+        js: `${projectFolder}/js/`,
+        img: `${projectFolder}/img/`,
+        svg: `${projectFolder}/svg/`,
+        fonts: `${projectFolder}/fonts/`,
+    },
+    src: {
+        html: [`${srcFolder}/**/*.html`, `!${srcFolder}/**/_*.html`],
+        scss: `${srcFolder}/scss/style.scss`,
+        ts: `${srcFolder}/ts/**/*.ts`,
+        js: `${projectFolder}/js/**/*.js`,
+        map: `${projectFolder}/js/**/*.map`,
+        img: `${srcFolder}/img/**/*.{jpg,png,svg,gif,ico,web}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        fonts: `${srcFolder}/fonts/*.ttf`,
+        fontsOtf: `${srcFolder}/fonts/*.otf`,
+    },
+    watch: {
+        html: `${srcFolder}/**/*.html`,
+        scss: `${srcFolder}/scss/style.scss`,
+        ts: `${srcFolder}/ts/**/*.ts`,
+        img: `${srcFolder}/img/**/*.{jpg,png,svg,gif,ico,web}`,
+    },
+    clean: `./${projectFolder}/`
+};
+
+const browserSync = () => {
+    browsersync.init({
+        server: {
+            baseDir: `./${projectFolder}/`,
+            // httpModule: "http2",
+            // https: true
+        },
+        port: 4000,
+        notify: false
+    });
+};
+
+const html = () =>
+    src(path.src.html)
+        .pipe(fileInclude())
+        .pipe(webpHtml())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+
+const images = () =>
+    src(path.src.img)
+        .pipe(webp({
+            quality: 70
+        }))
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({interlaced: true}),
+                imagemin.mozjpeg({progressive: true}),
+                imagemin.optipng({optimizationLevel: 5}),
+                imagemin.svgo({
+                    plugins: [
+                        {removeViewBox: false},
+                        {cleanupIDs: false}
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream());
+
+const clean = () => del(path.clean);
+
+const css = () =>
+    src(`${srcFolder}/scss/style.scss`)
+        .pipe(scss({outputStyle: "expanded"}).on("error", scss.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ["last 5 version"],
+            cascade: true
+        }))
+        .pipe(webpcss())
+        .pipe(groupMedia())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write())
+        .pipe(dest(path.build.css))
+        .pipe(cleanCss({level: 2}))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
+
+const fonts = () => {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts))
+        .pipe(browsersync.stream());
+
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts))
+        .pipe(browsersync.stream());
+};
+
+
+task("svgSprite", () => src([`${path.src.svg}`])
+    .pipe(svgSprite({
+        mode: {
+            stack: {
+                sprite: `./svg/icons.svg`,
+                example: true
+            }
+        }
+    }))
+    .pipe(dest(path.build.img)));
+
+task("otf2ttf", () => src([`${srcFolder}/fonts/**/*.otf`])
+    .pipe(fonter({
+        formats: ["ttf"]
+    }))
+    .pipe(dest(`${srcFolder}/fonts/`)));
+
+const bundleTs = () => {
+    const f =
+        browserify({
+            basedir: ".",
+            debug: true,
+            transform: [sourceify],
+            entries: ["src/ts/dynamicAdapt.ts"],
+            cache: {},
+            packageCache: {}
+        })
+            .plugin(tsify)
+            .bundle();
+
+    f.pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write("./"))
+        .pipe(dest(path.build.js));
+
+    return f.pipe(source("bundle.min.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
+};
+
+const watchFiles = () => {
+    watch([path.watch.html], html);
+    watch([path.watch.scss], css);
+    watch([path.watch.ts], bundleTs);
+    watch([path.watch.img], images);
+};
+
+const fontsStyle = (done: () => void) => {
+    fontsStl(srcFolder, path, fs);
+    done();
+};
+
+const build = series(clean, images, css, html, bundleTs, fonts, fontsStyle);
+const buildTs = series(clean, bundleTs);
+const watchServ = parallel(series(build, browserSync), watchFiles);
+
+exports.clean = clean;
+exports.build = build;
+exports.buildTs = buildTs;
+exports.default = watchServ;
---
+++ src/ts/dynamicAdapt.ts
@@ -0,0 +1,203 @@
+type TypeWidth = "min" | "max";
+type TypePlace = "first" | "last" | number;
+
+class DynamicAdaptItem {
+
+    private readonly _breakpoint: number;
+    private readonly _place: TypePlace;
+    private readonly _index: number;
+    private readonly _type: string;
+
+    static mobaleStartWidth = 767;
+    static planshetStartWidth = 992;
+
+    get type(): TypeWidth {
+        return (this._type !== "min" && this._type !== "max") ? "max" : this._type;
+    }
+
+    public get breakpoint(): number {
+        return this._breakpoint;
+    }
+
+    public get index(): number {
+        return this._index;
+    }
+
+    public get place(): number {
+        return DynamicAdaptItem.place2Index(this._place, this.destination);
+    }
+
+    static str2Num(value: string, defaultValue = 0): number {
+        return Number(value) || defaultValue;
+    }
+
+    static place2Index(value: TypePlace, destination: HTMLDivElement): number {
+        const res = Number(value);
+        if (res) {
+            return res;
+        }
+
+        if (value === "last") {
+            const children = Array.from(destination.children);
+            return Array.isArray(children) ? children.length : 0;
+        }
+
+        if (value === "first") {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    static indexInParent(parent: ParentNode | null, element: HTMLDivElement): number {
+        const array: HTMLDivElement[] = parent ? Array.prototype.slice.call(parent.children) : [];
+        return array.indexOf(element);
+    }
+
+    constructor(breakpoint: string,
+                public destination: HTMLDivElement,
+                public element: HTMLDivElement,
+                public parent: HTMLDivElement,
+                place: TypePlace,
+                type: string
+    ) {
+        this._breakpoint = DynamicAdaptItem.str2Num(breakpoint, DynamicAdaptItem.mobaleStartWidth);
+        this._index = DynamicAdaptItem.indexInParent(parent, element);
+        this._place = place;
+        this._type = type;
+    }
+}
+
+class DynamicAdapt {
+    // массив объектов
+    private dataDaNodes: DynamicAdaptItem[] = [];
+    private daClassname = "_dynamic_adapt_";
+
+    constructor() {
+        // наполнение objects объктами
+        document.querySelectorAll<HTMLDivElement>("[data-da]")
+            .forEach(node => {
+                if (node.dataset.da) {
+                    const dataArray: string[] = node.dataset.da.trim().split(",");
+
+                    const destination = document.querySelector<HTMLDivElement>(dataArray[0].trim());
+
+                    if (destination) {
+                        const dynamicAdaptItem = new DynamicAdaptItem(
+                            dataArray[2],
+                            destination,
+                            node,
+                            node.parentNode as HTMLDivElement,
+                            dataArray[1] ? dataArray[1].trim() as TypePlace : "last",
+                            dataArray.length === 3 ? dataArray[3] : "max"
+                        );
+
+                        this.dataDaNodes.push(dynamicAdaptItem);
+                    }
+                }
+            });
+
+        // this.arraySort(this.dataDaNodes);
+        // навешивание слушателя на медиа-запрос
+        // и вызов обработчика при первом запуске
+        this.dataDaNodes.map(item => `(${item.type}-width: ${item.breakpoint}px), ${item.breakpoint}`)
+            .forEach(media => {
+                const mediaSplit = media.split(",");
+                const matchMedia = window.matchMedia(mediaSplit[0]);
+                const mediaBreakpoint = Number(mediaSplit[1]);
+
+                // массив объектов с подходящим брейкпоинтом
+                const objectsFilter: DynamicAdaptItem[] = this.dataDaNodes.filter(_ => _.breakpoint === mediaBreakpoint);
+
+                matchMedia.addEventListener("change", () => {
+                    this.mediaHandler(matchMedia, objectsFilter);
+                });
+
+                this.mediaHandler(matchMedia, objectsFilter);
+            });
+    }
+
+
+    // Функция сортировки массива по breakpoint и place
+    // по возрастанию для this.type = min
+    // по убыванию для this.type = max
+    // private arraySort(arr: DynamicAdaptItem[]): void {
+    //     arr.sort((a: DynamicAdaptItem, b: DynamicAdaptItem): number => {
+    //         if (a.breakpoint === b.breakpoint) {
+    //             if (a.place === b.place) {
+    //                 return 0;
+    //             }
+    //
+    //             if (this.type === "min") {
+    //                 if (a.place < b.place) {
+    //                     return -1;
+    //                 }
+    //
+    //                 if (a.place > b.place) {
+    //                     return 1;
+    //                 }
+    //
+    //                 return a.place - b.place;
+    //             } else {
+    //                 if (b.place < a.place) {
+    //                     return -1;
+    //                 }
+    //
+    //                 if (b.place > a.place) {
+    //                     return 1;
+    //                 }
+    //
+    //                 return b.place - a.place;
+    //             }
+    //         }
+    //
+    //         if (this.type === "min") {
+    //             return a.breakpoint - b.breakpoint;
+    //         } else {
+    //             return b.breakpoint - a.breakpoint;
+    //         }
+    //     });
+    // }
+
+    private mediaHandler(matchMedia: MediaQueryList, dynamicAdaptItems: DynamicAdaptItem[]) {
+        dynamicAdaptItems.forEach(
+            dynamicAdaptItem => {
+                if (matchMedia.matches) {
+                    this.moveTo(dynamicAdaptItem.place, dynamicAdaptItem.element, dynamicAdaptItem.destination);
+                } else {
+                    if (dynamicAdaptItem.element.classList.contains(this.daClassname)) {
+                        this.moveBack(dynamicAdaptItem.parent, dynamicAdaptItem.element, dynamicAdaptItem.index);
+                    }
+                }
+            }
+        );
+    }
+
+    // Функция перемещения
+    private moveTo(place: number, element: HTMLDivElement, destination: HTMLDivElement) {
+        element.classList.add(this.daClassname);
+
+        if (place >= destination.children.length) {
+            destination.children[destination.children.length - 1].insertAdjacentElement("afterend", element);
+            return;
+        }
+
+        destination.children[place].insertAdjacentElement("beforebegin", element);
+    }
+
+    // Функция возврата
+    private moveBack(parent: HTMLDivElement, element: HTMLDivElement, index: number) {
+        element.classList.remove(this.daClassname);
+        const children = Array.from(parent.children);
+        if (children.length === 0) {
+            parent.insertAdjacentElement("beforeend", element);
+        } else if (children.length - 1 < index) {
+            parent.children[children.length - 1].insertAdjacentElement("afterend", element);
+        } else {
+            parent.children[children.length - 1].insertAdjacentElement("beforebegin", element);
+        }
+    }
+}
+
+new DynamicAdapt();
+
