1e592f2ae7f9b3d2a73f6f9b30baf78443483f66
uandtrew <uandrew1965@gmail.com>
2021-11-06 01:43:29 +0300

    .

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -36,14 +36,15 @@ export function fontsStl(source_folder: string, path: any, fs: any) {
     });
 }
 
-// function testWebP(callback: (support: boolean) => void) {
-//
-//     const webP = new Image();
-//     webP.onload = webP.onerror = function() {
-//         callback(webP.height == 2);
-//     };
-//     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-// }
+function testWebP(callback: (support: boolean) => void) {
+
+    const webP = new Image();
+    webP.onload = webP.onerror = function() {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
 //
 // testWebP(function(support: boolean) {
 //
