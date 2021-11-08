9b5b171109ed78a9a974f27e393ae2c2296273a4
Andrey Yudin <info@ivc-inform.ru>
2021-11-08 11:15:58 +0300

    .

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -45,7 +45,6 @@ function testWebP(callback: (support: boolean) => void) {
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
-//
 // testWebP(function(support: boolean) {
 //
 //     if (support === true) {
